67848b1686e947144e531b3033b6b5c9de775d4a
Sabha Parameswaran <sparameswaran@gopivotal.com>
2015-07-07 17:26:57 -0700

    Add Tile version support

--- ci/scripts/create-tile/run.sh
+++ ci/scripts/create-tile/run.sh
@@ -8,6 +8,7 @@ export RELEASE_NAME=${RELEASE_NAME:-appdirect-dev}
 export RELEASE_VERSION=${RELEASE_VERSION:-1.0}
 export RELEASE_FILE=${RELEASE_NAME}-${RELEASE_VERSION}.tgz
 
+export TILE_VERSION=${TILE_VERSION:-1.4}
 export TILE_RELEASE_NAME=p-${TILE_RELEASE_NAME:-$RELEASE_NAME}
 export TILE_RELEASE_VERSION=${TILE_RELEASE_NAME}-${PRODUCT_VERSION}
 
@@ -15,7 +16,8 @@ echo Creating a pivnet tile
 mkdir -p /tmp/tile-results/metadata /tmp/tile-results/releases
 
 cp $TASK_ROOT_DIR/release/$RELEASE_FILE /tmp/tile-results/releases
-cp $TASK_ROOT_DIR/repo/tile-config/metadata/*yml /tmp/tile-results/metadata
+#cp $TASK_ROOT_DIR/repo/tile-config/metadata/*yml /tmp/tile-results/metadata
+cp $TASK_ROOT_DIR/repo/*-${TILE_VERSION}.yml /tmp/tile-results/metadata
 
 cd /tmp/tile-results
 
