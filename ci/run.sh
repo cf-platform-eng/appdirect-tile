5ee93fd54685e0c321afc9883524ac4df4eee079
Sabha Parameswaran <sparameswaran@gopivotal.com>
2015-07-24 15:37:24 -0700

    Add CI Support and 1.5 Ops Mgr

--- ci/run.sh
+++ ci/run.sh
@@ -1,1 +1,1 @@
-fly configure -c scripts/pipeline.yml --vars-from ./config/default.yml --vars-from ./config/v1.5.yml --vars-from ./private.yml  appdirect-pipeline
+fly configure -c scripts/pipeline.yml --vars-from ./config/default.yml --vars-from ./config/v1.5.yml --vars-from ~/workspace/private.yml  appdirect-pipeline
